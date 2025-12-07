c9caa770cb96384f30e79a82dba9f7a22df5bda0
jw164 <jw164@illinois.edu>
2025-12-07 15:57:42 +0000

    ChoresList.jsx

--- frontend/src/pages/chores/ChoresList.jsx
+++ frontend/src/pages/chores/ChoresList.jsx
@@ -1,7 +1,32 @@
+import PageHeader from "../../components/PageHeader";
+import ChoreCard from "../../components/ChoreCard";
+import ChoreItem from "../../components/ChoreItem";
+
+const chores = [
+  {
+    name: "Take out trash",
+    assignedTo: "John",
+    dueDate: "Tomorrow",
+  },
+  {
+    name: "Wash dishes",
+    assignedTo: "Emily",
+    dueDate: "Today",
+  },
+];
+
 export default function ChoresList() {
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold">Chores List</h1>
-    </div>
-  )
+    <>
+      <PageHeader title="Chores List" />
+
+      <ChoreCard title="Chores">
+        <div className="space-y-2">
+          {chores.map((c, index) => (
+            <ChoreItem key={index} chore={c} />
+          ))}
+        </div>
+      </ChoreCard>
+    </>
+  );
 }
